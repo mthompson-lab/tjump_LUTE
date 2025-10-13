4e60af9af4c441aa8b8dafd034f9bcb1e73410c0
DorisMai <mai12345@sdfmilan128.sdf.slac.stanford.edu>
2025-10-13 16:11:36 -0700

    Fixed h5 file loading and tested on both psana1 (mfx101080524_Run0146.h5) and psana2 (limited, mfx101262725_Run0003.h5).

--- event_code_classifer.py
+++ event_code_classifer.py
@@ -8,7 +8,9 @@ import numpy as np
 from sklearn import metrics
 import pandas as pd
 import seaborn as sns
+import matplotlib
 from matplotlib import pyplot as plt
+import os
 from time import time
 from typing import Tuple,List
 start = time()
@@ -17,17 +19,22 @@ from argparse import ArgumentParser
 parser = ArgumentParser(__doc__)
 parser.add_argument('--folds', type=int, default=10, help="Number of crossvalidation folds with default 10")
 parser.add_argument('--num-jobs', type=int, default=-1, help="Number of cpu cores to use. By default use all available.")
-parser.add_argument('--image', type=str, default=None, help="Save a plot of the results to this file.")
-parser.add_argument('--csv', type=str, default=None, help="Save a csv of the results to this file.")
+parser.add_argument('--output-dir', type=str, default=".", help="Save the results to this directory. Default is the current directory.")
+parser.add_argument('--image', type=str, default=None, help="Save a plot of the results to this file (relative to output_dir). Default is None.")
+parser.add_argument('--csv', type=str, default=None, help="Save a csv of the results to this file (relative to output_dir). Default is None.")
 parser.add_argument('--show', action='store_true', help="Show the results plot interactively.")
 parser.add_argument('--event-codes', nargs='+', type=int, help="For h5 inputs, specify the event codes of interest.")
-parser.add_argument('data_file', help="A .npz or .h5 data file.")
+parser.add_argument('--data-file', help="A .npz or .h5 data file.")
 parser = parser.parse_args()
+
+# Set matplotlib backend based on whether we're showing plots interactively
+if not parser.show:
+    matplotlib.use('Agg')
 
 def load_npz_file(npz_file):
     data = np.load(npz_file, allow_pickle=True)
     label_names, labels = np.unique(data['label'], return_inverse=True)
-    x = np.concat((
+    x = np.concatenate((
         data['q'],
         data['i'],
     ), axis=-1)
@@ -39,30 +46,56 @@ def load_npz_file(npz_file):
 
 def load_h5_file(h5_file, event_codes):
     """
-    Load a smalldata, psana2 file
+    Load a smalldata, depending on the psana version
     """
     import h5py as h5
     f = h5.File(h5_file)
-    x = np.concat((
-        np.squeeze(f['jungfrau']['azav_azav']), #has an extra dimension
-        f['jungfrau']['azav_q'], #doesn't have any extra dimensions
-        ), axis=-1
-    )
+
+    # Determine psana version for h5 keys
+    if 'jungfrau' in f.keys():
+        psana_version = 2
+        azint_method = 'azav'
+        detname = 'jungfrau'
+        print(f"Detector jungfrau found in h5 key. Assuming psana version 2, and azimuthal integration method {azint_method}.")
+    else:
+        psana_version = 1
+        azint_method = 'pyfai'
+        detname = 'Rayonix'
+        print(f"Detector jungfrau not found in h5 key. Assuming psana version 1, with detector {detname} and azimuthal integration method {azint_method}.")
+
+    # Load azimuthal integration data
+    azav = f[detname][f'{azint_method}_azav']
+    if len(azav.shape) == 3:
+        azav = np.average(azav, axis=1) #2d azimuthal average
+    elif len(azav.shape) == 2:
+        pass
+    else:
+        raise ValueError(f"azav shape error: {azav.shape}")
+    q = f[detname][f'{azint_method}_q']
+    x = np.concatenate((azav, q), axis=-1) # shape (num_events, num_q * 2)
+    
+    # Load event code labels
     labels = []
-    label_names = []
-    for c in eventcodes:
-        label_names.append(f"Event Code {c}")
-        labels.append(
-            f['timing']['eventcodes'][:][:, c] == 1
-        )
-    labels = np.column_stack(labels)
+    label_names = ["Event Code {c}" for c in event_codes]
+    if psana_version == 1:
+        for c in event_codes:
+            labels.append(
+                f['evr'][f'code_{c}'][:] == 1
+            )
+    else:
+        for c in event_codes:
+            labels.append(
+                f['timing']['eventcodes'][:][:, c] == 1
+            )
+    labels = np.column_stack(labels) # one-hot encoded, shape (num_events, num_event_codes)
+    labels = np.argmax(labels, axis=1) # shape (num_events,)
     data = (
         x,
         labels
     )
     return data, label_names
 
-def load_data_file(data_file : str, **h5_kwargs) -> Tuple[Tuple['x' : np.ndarray, 'labels' : np.ndarray], 'label_names' : list[str]]:
+def load_data_file(data_file : str, **h5_kwargs) -> Tuple[Tuple[np.ndarray, np.ndarray], list[str]]:
     """
     Extract training data for the random forest classifier from an input file. 
 
@@ -109,7 +142,7 @@ folds = parser.folds
 num_jobs = parser.num_jobs
 
 seed = 42
-data,label_names = load_data_file(parser.data_file)
+data,label_names = load_data_file(parser.data_file, event_codes=parser.event_codes)
 
 l = data[0].shape[0]
 fold_ids = np.random.choice(folds, l)
@@ -140,9 +173,9 @@ stop = time()
 print(f"Runtime: {stop - start} seconds")
 
 if parser.csv is not None:
-    report.to_csv(parser.csv)
+    report.to_csv(os.path.join(parser.output_dir, parser.csv))
 if parser.image is not None:
-    plt.savefig(parser.image)
+    plt.savefig(os.path.join(parser.output_dir, parser.image))
 if parser.show:
     plt.show()
 
